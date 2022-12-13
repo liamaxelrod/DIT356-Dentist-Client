f8112a245162089bd87d848ed2749e8d9f762195
Liam Axelrod <liamaxelrod@gmail.com>
2022-12-13 16:09:16 +0100

    created register page
    the package has an input field for ID, password, company name, email
    And a button to actually create the account
    
    issue#15

---
+++ client/src/views/Register.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="center">
+        <div class="aboveButtons">
+            <p  id="pop-up">{{this.stateIfSuccessful}}</p>
+            <input id="inputID" v-model='changeIDText' placeholder="enter new ID">
+            <P></P>
+            <input id="inputpassword" v-model='changePasswordText' placeholder="enter new password">
+            <P></P>
+            <input id="inputCompany" v-model='changecompanyText' placeholder="enter new Company">
+            <P></P>
+            <input id="inputeEmail" v-model='changeEmailText' placeholder="enter new Email">
+            <P></P>
+            <button id="registerButton" class="btn btn-success btn-lg">register</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      changeIDText: '',
+      changePasswordText: '',
+      changecompanyText: '',
+      changeEmailText: '',
+      stateIfSuccessful: 'successful/failed to register'
+    }
+  },
+  mounted() {
+    /* blank for now */
+  },
+  methods: {
+    checkID() {
+      /* blank for now */
+    },
+    checkPassword() {
+      /* blank for now */
+    },
+    checkCompany() {
+      /* blank for now */
+    },
+    checkEmail() {
+      /* blank for now */
+    }
+  }
+}
+</script>
+
+<style>
+.center {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 70vh;
+    background-color: #80BAB2;
+    /*
+    height: 100%;
+    width: 100%;
+    problem with overall CSS app page I think for this part to work with the CSS
+    */
+}
+/* .pop-up {blank for now}  */
+#registerButton {
+    height: 100%;
+    width: 100%;
+}
+</style>
