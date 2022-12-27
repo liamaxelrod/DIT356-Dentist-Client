297c497a69c0544b3eb50fd8b1ee4f0efebbbdeb
Liam Axelrod <liamaxelrod@gmail.com>
2022-12-27 22:52:00 +0100

    updated user profile page
    I updated the user profile page and completed issue 8
    so now you can publish request to changes
    First name
    last name
    password
    company name
    email
    and delete account
    
    Issue #8

--- client/src/views/UserProfile.vue
+++ client/src/views/UserProfile.vue
@@ -1,117 +1,185 @@
 <template>
-  <div class="container">
-    <div class="leftSide">
-      <div>
-        <p></p>
-        <p id="infID">Dentists ID: {{accoountInfo.ID}}</p>
-        <p id="infPas">password: {{accoountInfo.password}}</p>
-        <p id="infCom">company: {{accoountInfo.company}}</p>
-      </div>
-      <div class="deleteAccount">
-        <button class="btn btn-danger" id="buttonDeleteAccount" @click="deleteAccount">DELETE Account</button>
-      </div>
+  <div class="background">
+   <div class="div1">
+    <p></p>
+      <p id="firstName">first name: {{accoountInfo.firstName}}</p>
+      <p id="lastName">last name: {{accoountInfo.lastName}}</p>
+      <p id="password">password: {{accoountInfo.password}}</p>
+      <p id="company">company: {{accoountInfo.company}}</p>
+      <p id="email">email: {{accoountInfo.email}}</p>
+    <div class="deleteAccount">
+      <button class="btn btn-danger" id="buttonDeleteAccount" @click="deleteAccount">DELETE Account</button>
     </div>
-    <div class="rightSide">
-      <div class="changeID">
-        <p></p>
-        <input v-model='changeIDText' placeholder="enter new ID">
-        <p></p>
-        <button class="btn btn-primary" id="buttonChangeIDButton" @click="changeID">changeID</button>
-      </div>
-      <div class="password">
-        <p></p>
-        <input v-model='changePasswordText' placeholder="enter new password">
-        <p></p>
-        <button class="btn btn-primary" id="buttonChangePassword" @click="changePassword">changeID</button>
-      </div>
-      <div class="company">
-        <p></p>
-        <input v-model='changeCompanyText' placeholder="enter new company name">
-        <p></p>
-        <button class="btn btn-primary" id="buttonChangeCompany" @click="changeCompany">changeID</button>
-      </div>
-    </div>
+   </div>
+   <div class="div2">
+    <p></p>
+    <input id="inputFirstName" v-model='changeFirstNameText' placeholder="enter new first name">
+    <input id="inputLastName" v-model='changeLastNameText' placeholder="enter new last name">
+    <input id="inputPassword" v-model='changePasswordText' placeholder="enter new password">
+    <input id="inputCompany" v-model='changeCompanyText' placeholder="enter new company">
+    <input id="inputEmail" v-model='changeEmailText' placeholder="enter new email">
+    <P></P>
+    <button id="bittpnChangeFirstName" @click="changeFirstName" class="btn btn-success btn-lg">change first name</button>
+    <button id="bittpnChangeLastName" @click="changeLastName" class="btn btn-success btn-lg">change last name</button>
+    <button id="bittpnChangePassword" @click="changePassword" class="btn btn-success btn-lg">change password</button>
+    <button id="bittpnChangeCompany" @click="changeCompany" class="btn btn-success btn-lg">change company</button>
+    <button id="bittpnChangeEmail" @click="changeEmail" class="btn btn-success btn-lg">change email</button>
+   </div>
   </div>
 </template>
 
 <script>
+import mymqtt from '../mymqtt'
+
 export default {
   data() {
     return {
-      changeIDText: '',
+      topic1: 'dentistimo/dentist/account/changes/first/name',
+      topic2: 'dentistimo/dentist/account/changes/last/name',
+      topic3: 'dentistimo/dentist/account/changes/password',
+      topic4: 'dentistimo/dentist/account/changes/company',
+      topic5: 'dentistimo/dentist/account/changes/email',
+      topic6: 'dentistimo/dentist/account/changes/delete',
+      changeFirstNameText: '',
+      changeLastNameText: '',
       changePasswordText: '',
       changeCompanyText: '',
+      changeEmailText: '',
+      mqtt_client: null,
       accoountInfo: {
-        ID: 'username',
+        firstName: 'firstName',
+        lastName: 'lastName',
         password: 'password',
-        company: 'company'
+        company: 'companyName',
+        email: 'email'
+
       }
     }
   },
-  mounted() {},
+  mounted() {
+    this.mqtt_client = mymqtt.createClient()
+    const msgCallback = (topic, message) => {
+      this.receive = message.toString()
+      this.context = message.toString()
+      console.log({ topic: topic, message: message.toString() })
+    }
+    this.mqtt_client.on('message', msgCallback)
+    this.mqtt_client.on('subscribe', (topic) => {
+      console.log('Subscribed too: ', topic)
+    })
+    // this.mqtt_client.subscribe(this.topic, { qos: 0 }, (error, res) => {
+    //   if (error) {
+    //     console.log('error = ', error)
+    //   } else {
+    //     console.log('res = ', res)
+    //   }
+    // })
+  },
   methods: {
-    changeID() {
-      this.accoountInfo.ID = this.changeIDText
+    changeFirstName() {
+      const payload = JSON.stringify({
+        dentistId: 'need dentistid',
+        category: 'first name',
+        change: this.changeFirstNameText
+      })
+      this.mqtt_client.subscribe(this.topic1, { qos: 0 }, (error, res) => {
+        if (error) { console.log('error = ', error) } else { console.log('res = ', res) }
+      })
+      const topic = 'dentistimo/dentist/breaks'
+      const qos = 0
+      this.mqtt_client.publish(topic, payload, qos)
+    },
+    changeLastName() {
+      const payload = JSON.stringify({
+        dentistId: 'need dentistid',
+        category: 'last name',
+        change: this.changeLastNameText
+      })
+      this.mqtt_client.subscribe(this.topic2, { qos: 0 }, (error, res) => {
+        if (error) { console.log('error = ', error) } else { console.log('res = ', res) }
+      })
+      const topic = 'dentistimo/dentist/breaks'
+      const qos = 0
+      this.mqtt_client.publish(topic, payload, qos)
     },
     changePassword() {
-      this.accoountInfo.password = this.changePasswordText
+      const payload = JSON.stringify({
+        dentistId: 'need dentistid',
+        category: 'password',
+        change: this.changePasswordText
+      })
+      this.mqtt_client.subscribe(this.topic3, { qos: 0 }, (error, res) => {
+        if (error) { console.log('error = ', error) } else { console.log('res = ', res) }
+      })
+      const topic = 'dentistimo/dentist/breaks'
+      const qos = 0
+      this.mqtt_client.publish(topic, payload, qos)
     },
     changeCompany() {
-      this.accoountInfo.company = this.changeCompanyText
+      const payload = JSON.stringify({
+        dentistId: 'need dentistid',
+        category: 'company',
+        change: this.changeCompanyText
+      })
+      this.mqtt_client.subscribe(this.topic4, { qos: 0 }, (error, res) => {
+        if (error) { console.log('error = ', error) } else { console.log('res = ', res) }
+      })
+      const topic = 'dentistimo/dentist/breaks'
+      const qos = 0
+      this.mqtt_client.publish(topic, payload, qos)
+    },
+    changeEmail() {
+      const payload = JSON.stringify({
+        dentistId: 'need dentistid',
+        category: 'email',
+        change: this.changeEmailText
+      })
+      this.mqtt_client.subscribe(this.topic5, { qos: 0 }, (error, res) => {
+        if (error) { console.log('error = ', error) } else { console.log('res = ', res) }
+      })
+      const topic = 'dentistimo/dentist/breaks'
+      const qos = 0
+      this.mqtt_client.publish(topic, payload, qos)
     },
     deleteAccount() {
-      // delete account
+      const payload = JSON.stringify({
+        dentistId: 'need dentistid',
+        delete: 'delete'
+      })
+      this.mqtt_client.subscribe(this.topic6, { qos: 0 }, (error, res) => {
+        if (error) { console.log('error = ', error) } else { console.log('res = ', res) }
+      })
+      const topic = 'dentistimo/dentist/breaks'
+      const qos = 0
+      this.mqtt_client.publish(topic, payload, qos)
     }
   }
 }
 </script>
 
-<style scoped>
-/* .container {} */
-.leftSide {
-  position: fixed;
-  width: 25%;
-  height: 100%;
-  left: 0;
-  background-color: #80BAB2;
-}
-.changeID,
-.password,
-.company {
-  display: inline-block;
-  margin: 10px;
-}
-#buttonChangeCompany,
-#buttonChangePassword,
-#buttonChangeIDButton {
+<style>
+.background {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row; /* won't turn into a row */
   height: 100%;
   width: 100%;
 }
-.rightSide {
-  position: fixed;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 75%;
-  height: 70vh;
-  right: 0;
+.div1 {
+  /* background:white; */
+  height: 100%;
+  width: 100%;
+  padding: 20px;
+  border: 10px
+  solid rgb(0, 255, 106);
 }
-#infID,
-#infPas,
-#infCom {
-  display: flex;
-  justify-content: center;
-  font-size: 20px;
-  color: rgb(255, 255, 255);
-}
-.deleteAccount {
-  bottom: 0;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-#buttonDeleteAccount {
-  color: rgb(255, 255, 255);
-  background-color: #ff0000;
+.div2 {
+  /* background: white; */
+  height: 100%;
+  width: 100%;
+  padding: 20px;
+  border: 10px
+  solid rgb(221, 255, 0);
 }
 </style>
