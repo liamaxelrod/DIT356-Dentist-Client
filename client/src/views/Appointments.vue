54c7c1eacfc7dedc99e8df6900009938a1c4db29
adrianni <adrianni@student.chalmers.se>
2022-12-29 09:10:31 +0100

    Displaying specific appointment information. issue#13

--- client/src/views/Appointments.vue
+++ client/src/views/Appointments.vue
@@ -7,31 +7,29 @@
       <!-- ADD INFORMATION IF THERE IS NO APPOINTMENT THAT DAY -->
       <b-calendar v-model="value" locale="en-EU"></b-calendar>
     </b-col>
-    <b-col>
-      <p>Value: <b>'{{ value }}'</b></p>
-    </b-col>
   </b-row>
-  <p onchange="myFunction" >| {{this.receive}} |</p>
   <b class="btn btn-dark" @click="appointments" style= "width: 17rem;">Get appointments</b>
   <div class="card border-info mb-3" style="width: 17rem;">
   <div class="card-header text-info">
     Appointment information </div>
-  <ul class="list-group list-group-flush text-info">
-    <li class="list-group-item">Client ID</li>
-    <li class="list-group-item">Date</li>
-    <li class="list-group-item">Time</li>
-  </ul>
-</div>
-<div class="card border-danger mb-3" style="width: 17rem;">
+        <ul class="list-group list-group-flush text-info">
+          <li class="list-group-item" v-bind="userid">Client ID: {{ userid }}</li>
+          <li class="list-group-item" v-bind="date">Date: {{ date }}</li>
+          <li class="list-group-item" v-bind="time">Time: {{ time }}</li>
+        </ul>
+<!--<div class="card border-danger mb-3" style="width: 17rem;">
   <div class="card-header text-danger">
     Cancel Appointment </div>
   <ul class="list-group list-group-flush text-info">
     <input id="inputDate" v-model="cancelDate" placeholder="Date of appointment YYYY-MM-DD"/>
     <input id="inputTime" v-model="cancelTime" placeholder="Time of appointment 00:00"/>
+    <b class="btn btn-danger" @click="cancelAppointments" style= "width: 17rem;">Cancel</b>
   </ul>
 </div>
+-->
     </div>
   </div>
+</div>
 </template>
 
 <script>
@@ -44,7 +42,9 @@ export default {
       mqtt_client: null,
       value: '',
       receive: '',
-      context: '',
+      userid: '',
+      date: '',
+      time: '',
       news: 'none',
       subscription: {
         topic: 'dentistimo/dentist-appointment/all-appointments-day',
@@ -55,8 +55,12 @@ export default {
   mounted() {
     this.mqtt_client = mymqtt.createClient()
     const msgCallback = (topic, message) => {
-      this.receive = message.toString()
-      this.context = message.toString()
+      const obj = JSON.parse(message.toString())
+      // console.log(obj)
+      // console.log(obj[0].dentistid)
+      this.userid = obj[0].userid
+      this.date = obj[0].date
+      this.time = obj[0].time
       console.log({ topic: topic, message: message.toString() })
     }
     this.mqtt_client.on('message', msgCallback)
@@ -83,6 +87,9 @@ Fetching the appointments of dentistid "xxxx". Next step is to incoprate it so i
       const topic = 'dentistimo/dentist-appointment/get-all-appointments-day'
       const qos = 0
       this.mqtt_client.publish(topic, payload, qos)
+    },
+    cancelAppointments() {
+
     },
     myFunction() {
       console.log('do something')
